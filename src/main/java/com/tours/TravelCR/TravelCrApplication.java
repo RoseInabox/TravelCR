1a65a628b8cbf671cb5a8ed2de93499f72cc3c58
joses <joses@DESKTOP-A3DSV72>
2026-03-11 21:50:14 -0600

    Inicio Proyecto

---
+++ src/main/java/com/tours/TravelCR/TravelCrApplication.java
@@ -0,0 +1,13 @@
+package com.tours.TravelCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCrApplication.class, args);
+	}
+
+}
